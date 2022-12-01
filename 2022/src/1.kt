e5624e81708ec192b22d5a2fb7242c40259fac8d
Freya Arbjerg <freya@arbjerg.dev>
2022-12-01 16:55:29 +0100

    Add day 1

---
+++ 2022/src/1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var currentElf = 0
+    val elves = mutableListOf<Int>()
+
+    getInput(1).lines().forEach { line ->
+        if (line.isBlank()) {
+            elves.add(currentElf)
+            currentElf = 0
+        } else currentElf += line.toInt()
+    }
+    elves.add(currentElf)
+
+    elves.sortDescending()
+    println(elves.take(3).sum())
+}
---
+++ 2022/src/util.kt
@@ -0,0 +1,3 @@
+import java.io.File
+
+fun getInput(day: Int) = File("2022/input/${day}.txt").readText()
