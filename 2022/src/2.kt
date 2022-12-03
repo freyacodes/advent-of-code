6dd34fa63c4d0d6a9eed7d13a7d537540037beda
Freya Arbjerg <freya@arbjerg.dev>
2022-12-03 20:21:39 +0100

    Rewrite day 2

--- 2022/src/2.kt
+++ 2022/src/2.kt
@@ -1,52 +1,26 @@
 fun main() {
-    var score = 0
-    getInput(2).lines().forEach { line ->
-        val characters = line.split(" ")
-        //val (them, us) = characters.map { fromLetter(it.first()) }
-        val them = fromLetter(characters.first().first())
-        val us = when(val targetOutcome = characters.last().first()) {
-            'X' -> them.winsAgainst
-            'Y' -> them
-            'Z' -> them.losesAgainst
-            else -> error("Unknown $targetOutcome")
-        }
-
-        val roundScore = us.playAgainst(them)
-        score += roundScore
-        println(line)
-        println("${us.toString().padEnd(7)} ${them.toString().padEnd(7)} " + roundScore)
-    }
-    println(score)
-}
-
-fun fromLetter(letter: Char): Hand {
-    return when (letter) {
-        'A', 'X' -> Hand.ROCK
-        'B', 'Y' -> Hand.PAPER
-        'C', 'Z' -> Hand.SCISSOR
-        else -> error("Invalid $letter")
-    }
-}
-
-enum class Hand(
-    private val value: Int,
-    private val winsAgainstOrdinal: Int,
-    private val losesAgainstOrdinal: Int
-) {
-    ROCK(1, 2, 1),
-    PAPER(2, 0, 2),
-    SCISSOR(3, 1, 0);
-
-    val winsAgainst by lazy { Hand.values()[winsAgainstOrdinal] }
-    val losesAgainst by lazy { Hand.values()[losesAgainstOrdinal] }
-
-    fun playAgainst(other: Hand): Int {
-        val winScore = when {
-            this.winsAgainst == other -> 6
-            this == other -> 3
-            else -> 0
-        }
-        println("$value $winScore")
-        return winScore + value
-    }
+    val scores1 = mapOf(
+        "A X" to 1 + 3,
+        "A Y" to 2 + 6,
+        "A Z" to 3 + 0,
+        "B X" to 1 + 0,
+        "B Y" to 2 + 3,
+        "B Z" to 3 + 6,
+        "C X" to 1 + 6,
+        "C Y" to 2 + 0,
+        "C Z" to 3 + 3
+    )
+    val scores2 = mapOf(
+        "A X" to 3 + 0,
+        "A Y" to 1 + 3,
+        "A Z" to 2 + 6,
+        "B X" to 1 + 0,
+        "B Y" to 2 + 3,
+        "B Z" to 3 + 6,
+        "C X" to 2 + 0,
+        "C Y" to 3 + 3,
+        "C Z" to 1 + 6
+    )
+    getInput(2).lines().sumOf { scores1[it]!! }.let { println("Part 1:  $it") }
+    getInput(2).lines().sumOf { scores2[it]!! }.let { println("Part 2:  $it") }
 }
