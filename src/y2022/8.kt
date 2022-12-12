984e759fcc32d54731d8a82a6aed72369b46918a
Freya Arbjerg <freya@arbjerg.dev>
2022-12-12 17:19:42 +0100

    Add Point2 class

--- src/y2022/8.kt
+++ src/y2022/8.kt
@@ -1,16 +1,16 @@
 package y2022
 
-private fun scanTrees(): Map<Pair<Int, Int>, Int> {
-    val map = mutableMapOf<Pair<Int, Int>, Int>()
+private fun scanTrees(): Map<Point2, Int> {
+    val map = mutableMapOf<Point2, Int>()
     getInput(8).mapIndexed { y, line ->
         line.forEachIndexed { x, c ->
-            map[x to y] = c.digitToInt()
+            map[Point2(x, y)] = c.digitToInt()
         }
     }
     return map
 }
 
-private fun partOne(trees: Map<Pair<Int, Int>, Int>) {
+private fun partOne(trees: Map<Point2, Int>) {
     val count = trees.count { (pos, height) ->
         val (x1, y1) = pos
         val north = trees.filterKeys { (x2, y2) -> x1 == x2 && y2 < y1 }
@@ -22,13 +22,13 @@ private fun partOne(trees: Map<Pair<Int, Int>, Int>) {
     println("Part one: $count")
 }
 
-private fun partTwo(trees: Map<Pair<Int, Int>, Int>) {
+private fun partTwo(trees: Map<Point2, Int>) {
     val count = trees.maxOf { (pos, height) ->
         val (x1, y1) = pos
-        val north = trees.filterKeys { (x2, y2) -> x1 == x2 && y2 < y1 }.toList().sortedByDescending { (p, _) -> p.second }
-        val south = trees.filterKeys { (x2, y2) -> x1 == x2 && y2 > y1 }.toList().sortedBy { (p, _) -> p.second }
-        val east = trees.filterKeys { (x2, y2) -> x1 < x2 && y2 == y1 }.toList().sortedBy { (p, _) -> p.first }
-        val west = trees.filterKeys { (x2, y2) -> x1 > x2 && y2 == y1 }.toList().sortedByDescending { (p, _) -> p.first }
+        val north = trees.filterKeys { (x2, y2) -> x1 == x2 && y2 < y1 }.toList().sortedByDescending { (p, _) -> p.y }
+        val south = trees.filterKeys { (x2, y2) -> x1 == x2 && y2 > y1 }.toList().sortedBy { (p, _) -> p.y }
+        val east = trees.filterKeys { (x2, y2) -> x1 < x2 && y2 == y1 }.toList().sortedBy { (p, _) -> p.x }
+        val west = trees.filterKeys { (x2, y2) -> x1 > x2 && y2 == y1 }.toList().sortedByDescending { (p, _) -> p.x }
         listOf(north, south, east, west).map { l ->
             l.takeWhile { (_, h) -> h < height }.size.plus(1).coerceAtMost(l.size)
         }.fold(1) { a, b -> a*b }
--- src/y2022/util.kt
+++ src/y2022/util.kt
@@ -4,3 +4,5 @@ import java.io.File
 
 fun getInput(day: Int) = File("src/y2022/input/${day}.txt").readLines()
 fun getInputString(day: Int) = File("src/y2022/input/${day}.txt").readText()
+
+data class Point2(val x: Int, val y: Int)
