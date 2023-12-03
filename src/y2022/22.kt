355b072db0f44b5d0436ce29045c07512fd47143
Freya Arbjerg <freya@arbjerg.dev>
2023-12-03 20:40:04 +0100

    Finish day 22 2022

--- src/y2022/22.kt
+++ src/y2022/22.kt
@@ -14,17 +14,78 @@ private enum class Heading(val offset: Point2) {
     UP(p(0, -1))
 }
 
-private fun wrap1(map: Map<Point2, MapTile>, position: Point2, heading: Heading): WrapResult {
+private fun wrapOne(map: Map<Point2, MapTile>, position: Point2, heading: Heading): WrapResult {
     val wrap = when (heading) {
         LEFT -> map.filter { it.key.y == position.y }.maxBy { it.key.x }
         RIGHT -> map.filter { it.key.y == position.y }.minBy { it.key.x }
         UP -> map.filter { it.key.x == position.x }.maxBy { it.key.y }
         DOWN -> map.filter { it.key.x == position.x }.minBy { it.key.y }
     }
-    return WrapResult(wrap.key, wrap.value, heading)
+    return WrapResult(wrap.key, heading)
 }
 
-private data class WrapResult(val position: Point2, val tile: MapTile, val heading: Heading)
+private fun wrapTwo(map: Map<Point2, MapTile>, position: Point2, heading: Heading): WrapResult {
+    val isSample = false
+    val sideLength = if (isSample) 4 else 50
+    val cubeIndex = (position) / sideLength
+    val (newCubeIndex, newHeading) = if (isSample) when (cubeIndex to heading) {
+        p(2, 1) to RIGHT -> p(3, 2) to DOWN
+        p(2, 2) to DOWN -> p(0, 1) to UP
+        p(1, 1) to UP -> p(2, 0) to RIGHT
+        else -> error("Missing: ${cubeIndex to heading}")
+    } else when (cubeIndex to heading) {
+        p(0, 2) to LEFT -> p(1, 0) to RIGHT
+        p(2, 0) to DOWN -> p(1, 1) to LEFT
+        p(1, 1) to RIGHT -> p(2, 0) to UP
+        p(2, 0) to RIGHT -> p(1, 2) to LEFT
+        p(1, 2) to RIGHT -> p(2, 0) to LEFT
+        p(0, 3) to DOWN -> p(2, 0) to DOWN
+        p(2, 0) to UP -> p(0, 3) to UP
+        p(1, 2) to DOWN -> p(0, 3) to LEFT
+        p(0, 3) to RIGHT -> p(1, 2) to UP
+        p(1, 1) to LEFT -> p(0, 2) to DOWN
+        p(1, 0) to LEFT -> p(0, 2) to RIGHT
+        p(1, 0) to UP -> p(0, 3) to RIGHT
+        p(0, 3) to LEFT -> p(1, 0) to DOWN
+        p(0, 2) to UP -> p(1, 1) to RIGHT
+        else -> error("Missing: ${cubeIndex to heading}")
+    }
+
+    /*
+    ...111444
+    ...222...
+    333666...
+    555......
+     */
+
+    val cubeX = position.x % sideLength
+    val cubeY = position.y % sideLength
+
+    val offset = when (heading) {
+        LEFT -> sideLength.dec() - cubeY
+        RIGHT -> cubeY
+        UP -> cubeX
+        DOWN -> sideLength.dec() - cubeX
+    }
+
+    val finalX = when (newHeading) {
+        LEFT -> sideLength.dec()
+        RIGHT -> 0
+        UP -> offset
+        DOWN -> sideLength.dec() - offset
+    } + sideLength * newCubeIndex.x
+
+    val finalY = when (newHeading) {
+        LEFT -> sideLength.dec() - offset
+        RIGHT -> offset
+        UP -> sideLength.dec()
+        DOWN -> 0
+    } + sideLength * newCubeIndex.y
+
+    return WrapResult(p(finalX, finalY), newHeading)
+}
+
+private data class WrapResult(val position: Point2, val heading: Heading)
 
 private fun parse(): Pair<Map<Point2, MapTile>, List<Pair<Char, Int>>> {
     val (upper, lower) = getInputStringUntrimmed(22).split("\n\n")
@@ -35,8 +96,8 @@ private fun parse(): Pair<Map<Point2, MapTile>, List<Pair<Char, Int>>> {
         line.forEachIndexed { x, c ->
             when (c) {
                 ' ' -> {}
-                '.' -> map[p(x+1,y+1)] = MapTile.OPEN
-                '#' -> map[p(x+1,y+1)] = MapTile.BLOCKED
+                '.' -> map[p(x, y)] = MapTile.OPEN
+                '#' -> map[p(x, y)] = MapTile.BLOCKED
             }
         }
     }
@@ -50,9 +111,8 @@ private fun parse(): Pair<Map<Point2, MapTile>, List<Pair<Char, Int>>> {
 
 private fun partOne(wrap: (Map<Point2, MapTile>, Point2, Heading) -> WrapResult): Int {
     val (map, commands) = parse()
-    var position = map.keys.filter { it.y == 1 }.minBy { it.x }
+    var position = map.keys.filter { it.y == 0 }.minBy { it.x }
     var heading = RIGHT
-    println(commands)
 
     commands.forEach { (lr, length) ->
         if (lr == 'L') heading = Heading.values()[(heading.ordinal - 1).mod(4)]
@@ -60,28 +120,31 @@ private fun partOne(wrap: (Map<Point2, MapTile>, Point2, Heading) -> WrapResult)
 
         repeat(length) {
             var newPosition = position + heading.offset
-            var next = map[newPosition]
+            var next = map[position + heading.offset]
             var newHeading = heading
             if (next == null) {
                 val res = wrap(map, position, heading)
                 newPosition = res.position
-                next = res.tile
+                next = map[res.position]!!
                 newHeading = res.heading
             }
 
             if (next == MapTile.BLOCKED) {
-                println("Position: $position, $heading")
+                //println("Position: $position, $heading")
                 return@forEach
             }
             position = newPosition
             heading = newHeading
         }
-        println("Position: $position, $heading")
+        //println("Position: $position, $heading")
     }
+
+    position += p(1, 1)
 
     return 1000 * position.y + 4 * position.x + heading.ordinal
 }
 
 fun main() {
-    println("Part one: " + partOne(::wrap1))
+    println("Part one: " + partOne(::wrapOne))
+    println("Part two: " + partOne(::wrapTwo))
 }
