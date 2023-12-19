baf66d2de0b68f9a2f5b23edab5e740e48e0ce5f
Freya Arbjerg <freya@arbjerg.dev>
2023-12-19 22:14:02 +0100

    Add day 19 part 2

--- src/y2023/19.kt
+++ src/y2023/19.kt
@@ -1,12 +1,14 @@
 package y2023
 
+//private var iteration = 0
 private data class PartValues(
     val x: Int,
     val m: Int,
     val a: Int,
     val s: Int
 ) {
-    operator fun get(c: Char) = when(c) {
+
+    operator fun get(c: Char) = when (c) {
         'x' -> x
         'm' -> m
         'a' -> a
@@ -14,7 +16,36 @@ private data class PartValues(
         else -> error("Panic! $c")
     }
 
-    val sum = x+m+a+s
+    val sum = x + m + a + s
+}
+
+private data class PartSequences(
+    val x: List<Int>,
+    val m: List<Int>,
+    val a: List<Int>,
+    val s: List<Int>,
+    val visited: Set<Condition> = emptySet()
+) {
+    //val i = iteration++
+    operator fun get(c: Char) = when (c) {
+        'x' -> x
+        'm' -> m
+        'a' -> a
+        's' -> s
+        else -> error("Panic! $c")
+    }
+
+    fun copy(c: Char, seq: List<Int>, condition: Condition): PartSequences {
+        return when (c) {
+            'x' -> copy(x = seq, visited = visited + condition)
+            'm' -> copy(m = seq, visited = visited + condition)
+            'a' -> copy(a = seq, visited = visited + condition)
+            's' -> copy(s = seq, visited = visited + condition)
+            else -> error("Panic!")
+        }
+    }
+
+    fun product() = x.count().toLong() * m.count().toLong() * a.count().toLong() * s.count().toLong()
 }
 
 private data class Workflow(val name: String, val conditions: List<Condition>, val elseThen: String)
@@ -25,6 +56,15 @@ private data class Condition(val target: Char, val lessThan: Boolean, val thresh
         val result = if (lessThan) value < threshold
         else value > threshold
         return if (result) ifTrue else null
+    }
+
+    fun check(parts: PartSequences): Pair<PartSequences, PartSequences> {
+        //println(parts.i)
+        val original = parts[target]
+        fun condition(n: Int) = if (lessThan) n < threshold else n > threshold
+        val trues = original.filter { condition(it) }
+        val falses = original.filter { !condition(it) }
+        return parts.copy(target, trues, this) to parts.copy(target, falses, this)
     }
 }
 
@@ -77,6 +117,48 @@ private fun partOne(): Int {
     }.sumOf { it.sum }
 }
 
+private var i = 0
+private fun resolve2(workflows: Map<String, Workflow>, workflowName: String, input: PartSequences): Long {
+    i++
+    if (input.product() == 0L) return 0
+    val workflow: Workflow
+    when (workflowName) {
+        "A" -> return input.product()
+        "R" -> return 0
+        else -> workflow = workflows[workflowName]!!
+    }
+
+    if (workflow.conditions.first() in input.visited) return input.product()
+
+    var sum = 0L
+    var nextInput = input
+    workflow.conditions.forEach { condition ->
+        val (trues, falses) = condition.check(nextInput)
+        nextInput = falses
+        sum += resolve2(workflows, condition.ifTrue, trues)
+    }
+
+    return sum + resolve2(workflows, workflow.elseThen, nextInput)
+}
+
+private fun partOneAlt(): List<Long> {
+    val (workflows, inputs) = parse()
+    return inputs.map {
+        it.run {
+            resolve2(workflows, "in", PartSequences((x..x).toList(), (m..m).toList(), (a..a).toList(), (s..s).toList()))
+        }
+    }
+}
+
+private fun partTwo(): Long {
+    val (workflows, _) = parse()
+    val initial = PartSequences((1..4000).toList(), (1..4000).toList(),
+        (1..4000).toList(), (1..4000).toList())
+
+    return resolve2(workflows, "in", initial)
+}
+
 fun main() {
     println("Part one: " + partOne())
+    println("Part two: " + partTwo())
 }
