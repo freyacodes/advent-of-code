5ca657aac97d00620e594941755ada30b2849fc0
Freya Arbjerg <freya@arbjerg.dev>
2023-12-23 22:48:36 +0100

    Add day 21 part 2

--- src/y2023/21.kt
+++ src/y2023/21.kt
@@ -2,6 +2,7 @@ package y2023
 
 import y2022.Point2
 import y2022.p
+import java.lang.StrictMath.pow
 
 private val north = p(0, -1)
 private val south = p(0, 1)
@@ -39,7 +40,7 @@ private class GardenNode(val map: Map<Point2, Char>, val point2: Point2) {
     override fun hashCode() = point2.hashCode()
 }
 
-private fun solveSlow(map: Map<Point2, Char>, steps: Int, start: Point2): Long {
+private fun solveSlow(map: Map<Point2, Char>, steps: Int, start: Point2 = p(65, 65)): Long {
     var lastVisited = setOf(GardenNode(map, start))
     repeat(steps) {
         lastVisited = lastVisited.flatMap { it.getNeighbors() }.toSet()
@@ -53,63 +54,53 @@ private fun partOne(): Long {
     return solveSlow(map, 64, start)
 }
 
-private fun partTwo(stepsRequired: Long = 26501365): Long {
-    val map = parse()
-    val start = map.entries.single { it.value == 'S' }.key
-
-    val sideLength = 131
-    val halfLength = sideLength / 2
-    val mapDistanceFromCenter = (stepsRequired - halfLength + 1) / sideLength
-    val fullyCoveredMaps = diamondArea(mapDistanceFromCenter.toInt())
-    val partialMapsPerDiagonal = (diamondArea(mapDistanceFromCenter.toInt() + 1) - fullyCoveredMaps) / 4
-
-    val coveredSector = solveSlow(map, sideLength * 2 + 1, p(halfLength, halfLength))
-
-    val northSector = solveSlow(map, halfLength, p(halfLength, 0))
-    val eastSector = solveSlow(map, halfLength, p(sideLength - 1, halfLength))
-    val southSector = solveSlow(map, halfLength, p(halfLength, sideLength - 1))
-    val westSector = solveSlow(map, halfLength, p(0, halfLength))
-
-    val diagonalCoverage = listOf(
-        solveSlow(map, sideLength - 1, p(0, 0)),
-        solveSlow(map, sideLength - 1, p(sideLength - 1, 0)),
-        solveSlow(map, sideLength - 1, p(0, sideLength - 1)),
-        solveSlow(map, sideLength - 1, p(sideLength - 1, sideLength - 1))
-    ).sum() * partialMapsPerDiagonal
-
-    // 34995 Verification
-
-    return northSector + eastSector + southSector + westSector + diagonalCoverage + coveredSector * fullyCoveredMaps
-}
-
-fun verificationTest(): Long {
-    val delta = p(131, 131)
-    val offsets = listOf(
-        p(-1, -1), p(0, -1), p(1, -1),
-        p(-1, 0), p(1, 0),
-        p(-1, 1), p(0, 1), p(1, 1)
-    )
+private fun generateLargeMap(): Map<Point2, Char> {
     val input = parse()
-    val map = offsets.map { offset ->
-        input.mapKeys { it.key + (offset * 131) }
-    }.reduce { acc, map -> acc + map } + input
+    val map = mutableMapOf<Point2, Char>()
+    for (x in -5..5) {
+        for (y in -5..5) {
+            map.plusAssign(input.mapKeys { it.key + (p(x, y) * 131) })
+        }
+    }
 
-    return solveSlow(map, 131 + 64, p(65, 65))
+    return map
 }
 
-fun diamondArea(length: Int): Long {
-    if (length == 1) return 1
-    var value = 1L
-    for (n in 2..length) {
-        value += n * 2 + (n-1) * 2 - 2
-    }
-    return value
+private fun partTwo(stepsRequired: Long = 26501365): Long {
+    val map = generateLargeMap()
+
+    val y0 = solveSlow(map, 65)
+    println(y0)
+    val y1 = solveSlow(map, 65 + 131)
+    println(y1)
+    val y2 = solveSlow(map, 65 + 131*2)
+    println(y2)
+
+    // G(x) = ax² + bx + c
+    // (a * 0²) + (b * 0) + c = y0
+    val c = y0
+
+    // G(1) = (a * 1²) + (b * 1) + c
+    // a + b + y0 = y1
+    // b = y1 - y0 - a
+
+    // G(2) = (2a)² + 2b + y0 = y2
+    // 4a + 2b = y2 - y0
+    // 4a + 2(y1 - y0 - a) = y2 - y0
+    // 2a + 2y1 - 2y0 = y2 - y0
+    // 2a + 2y1 - y0 = y2
+    // 2a = y2 + y0 - 2y1
+    // a = (y2 + y0 - 2 * y1) / 2
+    val a = ((y2 + y0 - (2 * y1)) / 2).toLong()
+    val b = (y1 - y0 - a).toLong()
+
+    val g = 639051580070841
+
+    val x = (stepsRequired - 65) / 131L
+    return a * pow(x.toDouble(), 2.0).toLong() + b * x + c
 }
 
 fun main() {
-    println(parse().filter { it.value == '.' }.count()/2)
-    println(parse().filter { it.value == '.' }.count()/2 * (1 + 4 / 2 + 1))
     println("Part one: " + partOne())
-    println("Part two: " + partTwo())//131 + 131/2)) // 37196050414 too low, not 638107177748370
-    println("Verification: " + verificationTest())
+    println("Part two: " + partTwo())
 }
